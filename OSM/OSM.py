dd20deb5e6d349566e55a4c1ec18a30afad5b6e5
mateoxc <48658632+mateoxc@users.noreply.github.com>
2022-03-15 22:18:31 +0100

    up

--- OSM/OSM.py
+++ OSM/OSM.py
@@ -1,26 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import osmnx as ox
 import pandas as pd
 
 
 
-def out_osm(place,tags):
-    df = ox.geometries_from_place(place, tags=tags)
+def out_osm(place,tags,fname):
+    df = ox.geometries_from_place(place, tags=tags,)
+    ox.downloader.overpass_request(df, pause=None)
+
     #df = df[df.geom_type == 'Point'][:]
+    '''Wybór poszczególnych kolumn
     keys=['name','geometry']
     for key in tags.keys():
         keys.append(str(key))
         #print(keys)
     df=df[keys]
+    '''
+    df.to_csv(fname,encoding='utf8') 
     return df
     
 
 place = 'Warsaw, PL'
-tags = {'amenity': 'bicycle_rental','public_transport': 'station','cycleway':'lane'}
-df=out_osm(place,tags)
+tags = {'amenity': ['bicycle_rental','bicycle_parking','bicycle_repair_station','bus_station']
+        ,'building':['train_station','transportation']
+        ,'public_transport': ['station','stop_position','platform','station','stop_area']
+        ,'highway':['bus_stop']
+        ,'railway':['halt','platform','station','tram_stop']}
+
+tags_lines={'highway':['busway','cycleway']
+            ,'cycleway':['lane','opposite','opposite_lane','track','opposite_track','share_busway','opposite_share_busway','shared_lane']
+            ,'busway	':['lane']
+            ,'railway':['light_rail','monorail','rail','subway','tram']
+            ,'route	':['bicycle','bus','light_rail','railway','tram']}
+df=out_osm(place,tags,'poi.csv')
